3ecf842a9dfcafb24d4dedca2853dadf1f2280f6
jusmims0 <102726528+jusmims0@users.noreply.github.com>
2022-06-07 08:51:22 +0200

    Add project files.

---
+++ Assignment6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Assignment6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = "Learning Programming is cool";
+
+            string[] words = text.Split(' ');
+
+            int longestWordLength = 0;
+            string longestWord = "";
+
+
+            for(int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length > longestWordLength)
+                { 
+                    longestWordLength = words[i].Length;
+                    longestWord = words[i];
+                }
+            }
+
+            Console.WriteLine($"The longest word is {longestWord}");
+        }
+    }
+        
+}
